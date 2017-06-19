4e157b214621e60060db2035720fb28cadb4d2d8
Thomas Gossart <tg.59290@gmail.com>
2017-06-19 11:46:31 +0200

    added the js saved on trello

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+// Store the paths of your image in an array and the associated descriptions
+       var paths = ["img/background.jpg","img/supports.jpg", "img/temoignage_droite.jpg"];
+       var descriptions = ["first description", "second description", "third description"];
+       var maxIndex = paths.length - 1;
 
+       // Get the image and the description paragraphe from the DOM
+       var imageContainer = document.getElementById("slider_image");
+       var description = document.getElementById("description");
+
+       // Set the image and the description according to the slider index
+       function setCurrentContent() {
+         imageContainer.src = paths[currentIndex];
+         description.innerHTML = descriptions[currentIndex];
+       }
+
+       // Function to update the index and display the associated image and description
+       function setImage () {
+         currentIndex < maxIndex ? currentIndex +=1 : currentIndex = 0;
+         setCurrentContent();
+       }
+
+       // Initialize the slider with the first image and description
+       var currentIndex = 0;
+       setCurrentContent();
+
+       // Launches the setimage function every 3 secondes
+       var sliderTurn = setInterval(setImage, 3000);
+
+       // Function to show next or previous content
+       function show(direction) {
+         clearInterval(sliderTurn);
+         direction === true ? (currentIndex < maxIndex ? currentIndex ++ : currentIndex = 0) : (currentIndex > 0 ? currentIndex -- : currentIndex = 2);
+         setCurrentContent();
+         sliderTurn = setInterval(setImage, 3000);
+       }
